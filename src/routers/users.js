d745e03558faa4b0ac1cc70b0c26e942b48f4c58
Samarpan Koirala <samarpankoirala@gmail.com>
2025-03-02 12:32:49 -0600

    added image upload for user profile and feature for serving the images

--- src/routers/users.js
+++ src/routers/users.js
@@ -4,7 +4,6 @@ const auth = require("../middleware/auth");
 const User = require("../models/user");
 const multer = require("multer");
 const upload = multer({
-  dest: "avatar",
   limits: {
     fileSize: 1000000,
   },
@@ -98,8 +97,37 @@ router.delete("/users/me", auth, async (req, res) => {
   }
 });
 
-router.post("/users/me/avatar", upload.single("avatar"), (req, res) => {
+router.post(
+  "/users/me/avatar",
+  auth,
+  upload.single("avatar"),
+  async (req, res) => {
+    req.user.avatar = req.file.buffer;
+    await req.user.save();
+    res.send();
+  },
+  (error, req, res, next) => {
+    res.status(400).send({ error: error.message });
+  }
+);
+
+router.delete("/users/me/avatar", auth, async (req, res) => {
+  req.user.avatar = undefined;
+  await req.user.save();
   res.send();
+});
+
+router.get("/users/:id/avatar", async (req, res) => {
+  try {
+    const user = await User.findById(req.params.id);
+    if (!user || !user.avatar) {
+      throw new Error("Could not fetch avatar");
+    }
+    res.set("Content-Type", "image/png");
+    res.send(user.avatar);
+  } catch (e) {
+    res.status(404).send(e);
+  }
 });
 
 module.exports = router;
